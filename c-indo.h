5aa32cfd4067722521fac6a97f009c521fab0ce5
mee-i <110971039+mee-i@users.noreply.github.com>
2023-03-16 21:00:07 +0700

    Add files via upload

---
+++ c-indo.h
@@ -0,0 +1,68 @@
+// C-Indo.h by mee-i
+
+#pragma once
+#include <iostream>
+#include <math.h>
+#include <chrono>
+#include <thread>
+
+#define bilangan_bulat int
+#define bulat int
+#define panjang long
+#define pendek short
+#define apakah bool
+#define karakter char
+#define kar char
+#define apung float
+#define dobel double
+#define konstan const
+#define benar true
+#define salah false
+#define struktur struct
+#define ruang void
+#define kelas class
+#define publik public
+#define pribadi private
+#define tali std::string
+#define rangkaian std::string
+
+#define kurang < // kurang dari
+#define kurangsama <= // kurang dari atau sama dari
+#define lebih > // lebih dari
+#define lebihsama >= // lebih dari atau sama dari
+#define adalah ==
+#define bukan !=
+#define atau ||
+#define maka {
+#define ini {
+#define lalu {
+#define akhir }
+
+#define masuk <<
+#define keluar >>
+#define baris std::cout<<"\n"
+#define cetak std::cout<<
+#define cetakf printf
+#define kembali return
+#define untuk for
+#define jika if
+#define lain else
+#define lainnya else
+#define ganti switch
+#define hal case
+#define hapus std::erase
+#define BATAL NULL
+#define ketika while
+#define istirahat break
+#define akar sqrt
+#define coba try
+#define tangkap catch
+#define lempar throw
+#define rangkaian_ke_bulat std::stoi
+#define ke_rangkaian std::to_string
+#define pengecualian std::exception
+#define apa what()
+
+void tunda(int milidetik) { // buat fungsi tunda untuk memnunda program
+	std::this_thread::sleep_for(std::chrono::milliseconds(milidetik));
+}
---
+++ contoh_program.cpp
@@ -0,0 +1,98 @@
+// CIndo.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "c-indo.h"
+#include <thread>
+
+bulat kali(bulat nilai_a, bulat nilai_b) maka
+    kembali nilai_a * nilai_b; // beri kembali nilai a dikali oleh nilai b
+akhir
+
+int main() {
+    cetak "apakah dia siswa?\n";
+    tunda(1000);
+    tali inisiswa = "dia adalah siswa\n";
+    rangkaian bukansiswa = "dia bukan siswa\n";
+    apakah siswa = salah; // benar atau salah
+    
+    jika (siswa adalah benar) maka
+        cetak inisiswa;
+    akhir lain maka
+        cetak bukansiswa;
+    akhir
+
+    cetak "\n\n"; // "\n" untuk menambahkan baris baru
+    tunda(1000);
+    ////////////////////////////////// Pertambahan //////////////////////////////////
+    bulat a = 1;
+    bilangan_bulat b = 1;
+    bulat hasil;
+    hasil = a + b;
+    cetak "hasil dari a + b adalah : " masuk hasil;
+    /////////////////////////////////////////////////////////////////////////////////
+    baris; // buat 1 baris baru
+    baris; // buat baris lagi
+    tunda(1000);
+    ////////////////////////////////// Perkalian ////////////////////////////////////
+    cetak "perkalian 12 x 10: " masuk kali(12, 10);
+    /////////////////////////////////////////////////////////////////////////////////
+
+    baris;
+    baris;
+    tunda(1000);
+
+    ///////////////////////////// Percobaan Kesalahan ///////////////////////////////
+    cetak "mencoba untuk melakukan kesalahan...\n";
+    coba ini
+        std::thread().detach();
+    akhir
+    tangkap (pengecualian &error) lalu
+        cetak "ada kesalahan disini : " masuk error.apa;
+    akhir
+    /////////////////////////////////////////////////////////////////////////////////
+
+    baris;
+    baris; // menambahkan 2 baris baru
+
+    /////////////////////////////// Perputaran (Loop) ///////////////////////////////
+    cetak "while loop";
+    apakah berputar = benar;
+    bulat putaran = 0;
+    ketika(berputar adalah benar) maka // putaran (loop) terus menerus apabila boolean berputar adalah benar
+        baris;
+        putaran += 1; // tambah 1 setiap putaran
+        jika(putaran lebih 10) maka // jika lebih dari 10 maka
+            berputar = salah; // mengubah boolean berputar menjadi salah agar putaran (loop) berhenti
+        akhir lainnya maka
+            cetak "putaran ke " masuk putaran;
+        akhir
+        tunda(500); // tunda program selama 0.5 detik (500 milidetik)
+    akhir
+
+            baris; baris;
+
+    cetak "for loop";
+    untuk(int c = 1; c < 11; c++) maka
+        baris;
+        cetak "putaran ke " masuk c; 
+        tunda(500); // tunda program selama 0.5 detik (500 milidetik)
+    akhir
+    /////////////////////////////////////////////////////////////////////////////////
+
+
+    baris;
+    baris;
+
+    //////////////////////////////////// Akar ///////////////////////////////////////
+    bulat d = 25;
+    cetak "akar dari 25 adalah " masuk akar(d);
+    /////////////////////////////////////////////////////////////////////////////////
+
+    baris;
+    baris;
+    cetak "program telah selesai, menutup dalam 5 detik...";
+    tunda(5000); // tunda program 5 detik
+
+    kembali 0;
+}
